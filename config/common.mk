OdSazib/android_vendor_awaken-1

vendor: fonts: Add Google Sans font and change output dir
Change-Id: I7fd353cb2991caa1cb9b7b5dfed86225db44ef86
Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

config/common.mk
@@ -72,6 +72,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=log
 
+# Fonts
+include vendor/awaken/config/fonts.mk
+
 # Gapps
 $(call inherit-product, vendor/awaken/config/gapps.mk)
 

username0136/vendor_genesis

config/common.mk
@@ -80,6 +80,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.lineageos.android.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.android.xml
 
+# Include font files
+include vendor/lineage/config/fonts.mk
+
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
 